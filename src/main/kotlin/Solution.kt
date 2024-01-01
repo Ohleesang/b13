fbc68061a7c05e73c10936f48b73f273832e6182
Ohleesang <jammmmed@gmail.com>
2024-01-01 23:10:02 +0900

    2중 반복문

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,2 +1,30 @@
+
+
 class Solution {
+    fun solution(num: Int, total: Int): IntArray {
+        var answer: IntArray = intArrayOf()
+        var sum = 0
+        var temp = 0
+        val arr = Array<Int>(num) { 0 }
+        for(j in -1000 .. 1000) {
+            for (i in 1..num) {
+                temp = i+j
+                sum += temp
+                arr[i-1] = temp
+            }
+            if(sum==total) return arr.toIntArray()
+            sum = 0
+
+        }
+
+        return answer
+    }
+}
+
+fun main() {
+    var a = Solution()
+    a.solution(3, 12)//3,4,5
+    a.solution(5, 15)//1,2,3,4,5
+    a.solution(4,14)//2,3,4,5
+    a.solution(5,5)//-1,0,1,2,3
 }
